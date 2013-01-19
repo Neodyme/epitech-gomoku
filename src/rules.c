174c0d4b794cc5de42081ce31ec11591561b8b3c
prost p <you@example.com>
2013-01-19 19:31:51 +0100

    typo

--- src/rules.c
+++ src/rules.c
@@ -63,7 +63,7 @@ long	drec(t_board *board, int color, long d, int sen, register unsigned int x, r
 
 long	longdrec(t_board *board, int color, long d, int sen, register unsigned int x, register unsigned int y)
 {
-  ~,.,,,mmmmmmmmmmmmmmmmmm  if (get_board(board, x, y) == (OPPOSITE(color)))
+  if (get_board(board, x, y) == (OPPOSITE(color)))
     {
        ((char*)&d)[sen & 0x0f] |= BLOCKED;
       return (d);
